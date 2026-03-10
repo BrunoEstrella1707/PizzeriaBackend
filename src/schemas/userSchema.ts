f12aee4112de11ff149cc88cb1ddc66edae0cfeb
brunoestrella <brunogamer2002@gmail.com>
2026-03-10 16:20:36 -0300

    <feat: user schema>

---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+
+export const createUserSchema = z.object({
+    body: z.object({
+        name: z.string().min(3, {message: "Name must have more than 2 letters"}),
+        email: z.email(),
+        password: z.string().min(6, {message: "Password must be 6 character length"})
+    }),
+})
