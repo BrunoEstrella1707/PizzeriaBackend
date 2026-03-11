6c548a50ff956e91634223ce7005715a7af9a2b0
brunoestrella <brunogamer2002@gmail.com>
2026-03-11 15:46:15 -0300

    <feat: creating auth user schema>

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -4,7 +4,14 @@ import { z } from 'zod'
 export const createUserSchema = z.object({
     body: z.object({
         name: z.string().min(3, {message: "Name must have more than 2 letters"}),
-        email: z.email(),
+        email: z.email({message: "Enter a valid email."}),
         password: z.string().min(6, {message: "Password must be 6 character length"})
+    }),
+})
+
+export const authUserSchema = z.object({
+    body: z.object({
+        email: z.email({message: "Enter a valid email."}),
+        password: z.string({message: "Password must be provided"})
     }),
 })
