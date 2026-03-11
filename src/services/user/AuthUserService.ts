d61028d40280bb0d784801da08d478ab06d19378
brunoestrella <brunogamer2002@gmail.com>
2026-03-11 16:27:01 -0300

    <feat: providing jwt token for auth user>

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,6 +1,9 @@
+import { compare } from 'bcryptjs'
 import prismaClient from  '../../prisma/index'
-import { hash } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 
+
+const JWT_SECRET = process.env.JWT_SECRET as string || 'secret_key'
 
 interface AuthUserProps {
     email: string
@@ -9,8 +12,36 @@ interface AuthUserProps {
 
 class AuthUserService{
     async execute({ email, password }: AuthUserProps){
-        console.log({ email, password })
-        return 'Logado'
+
+        const user = await prismaClient.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+
+        if (!user){
+            throw new Error("User email not found!")
+        }
+
+        const passwordMatch = await compare(password, user.password)
+
+        if (!passwordMatch){
+            throw new Error("The passwords didnt match!")
+        }
+
+        const token = sign(
+            {id: user.id},
+            JWT_SECRET,
+            { expiresIn: '10m'},
+        )
+
+       return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: token,
+       }
     }
 }
 
