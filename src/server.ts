cc4448025d5e1233e4aff76ea68420643a46306e
brunoestrella <brunogamer2002@gmail.com>
2026-03-11 16:29:23 -0300

    <fix: next function added to error handling>

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import cors from 'cors'
 import 'dotenv/config'
-import express, { Response, Request } from 'express'
+import express, { Response, Request, NextFunction } from 'express'
 import { router } from './routes'
 
 
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors())
 app.use(router)
 
-app.use((error: Error, req: Request, res: Response) => {
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
 
     if (error instanceof Error){
         return res.status(400).json({
