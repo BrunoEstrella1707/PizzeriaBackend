46d846f5255c4d1a8b3f8bb0b1543dceaac0233d
brunoestrella <brunogamer2002@gmail.com>
2026-03-12 16:27:58 -0300

    <feat: authentication middleware>

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { verify } from 'jsonwebtoken'
+import { Request, Response, NextFunction } from 'express'
+
+
+interface Payload{
+    id: Number
+}
+
+const JWT_SECRET = process.env.JWT_SECRET as string || 'secret_key'
+
+export function auth(req: Request, res: Response, next: NextFunction){
+
+    const authToken = req.headers.authorization
+
+    if (!authToken){
+        res.status(401).json({ error: "Token not provided" })
+    }
+
+    try{
+
+        const { id } = verify(authToken!.replace('Bearer ', ''), JWT_SECRET) as Payload
+
+        req.user_id = id
+        
+        next()
+
+    } catch(error){
+        res.status(401).json({ error: "Unauthorized" })
+    }
+}
+
