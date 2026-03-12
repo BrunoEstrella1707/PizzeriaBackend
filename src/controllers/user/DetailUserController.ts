4be1d52de136fbd430e0a182a2e6ed2555785de1
brunoestrella <brunogamer2002@gmail.com>
2026-03-12 16:28:10 -0300

    <feat: user detail controller>

---
+++ src/controllers/user/DetailUserController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+import { DetailUserService } from "../../services/user/DetailUserService"
+
+
+class DetailUserController{
+    async handle(req: Request, res: Response) {
+
+        const user_id = req.user_id as number
+
+        const detailUserService = new DetailUserService()
+
+        const user = await detailUserService.execute(user_id)
+
+        res.json(user)
+
+    }
+}
+
+
+class DetailAnotherUserController{
+    async handle(req: Request, res: Response) {
+
+        const user_id = req.params.userId
+
+        const detailUserService = new DetailUserService()
+
+        const user = await detailUserService.execute(Number(user_id))
+
+        res.json(user)
+
+    }
+}
+
+export { DetailUserController, DetailAnotherUserController}
