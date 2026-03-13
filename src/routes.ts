e7aef91c2e5170cada497942e18015661cca1fcf
brunoestrella <brunogamer2002@gmail.com>
2026-03-13 14:15:35 -0300

    <feat: adding category create route>

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController, DetailAnotherUserController } from './controllers/user/DetailUserController'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
 import { validateSchema } from './middlewares/validateSchema'
 import { authUserSchema, createUserSchema } from './schemas/userSchema'
 import { auth } from './middlewares/auth'
@@ -31,6 +32,12 @@ router.get(
     '/users/:userId',
     auth,
     new DetailAnotherUserController().handle
+)
+
+router.post(
+    '/categories',
+    auth,
+    new CreateCategoryController().handle
 )
 
 export { router }
